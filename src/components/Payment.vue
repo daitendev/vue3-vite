b589d3108de70fcca1df777344a441e95517d91f
daitendev <18205123+daitendev@users.noreply.github.com>
2023-02-02 20:42:12 +0900

    import ref & reactive for vue3

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,12 +1,41 @@
 <script setup lang="ts">
+import { ref, reactive } from 'vue'
+const itemName1 = ref<string>('desk')
+const itemName2 = 'bike'
+
+const item1 = reactive(
+  {
+    name: 'desk',
+    price: 40000
+  }
+)
+
+const buy = (itemName: string) => {
+  alert('Are you sure to buy?' + itemName)
+}
+
+const input = (event: any) => {
+  item1.name = event.target.value
+}
+const inputPrice = (event: any) => {
+  item1.price = event.target.value
+}
 </script>
 
 <template>
   <div class="container">
-    <h1>最近の支出</h1>
+    <h1>Payment</h1>
+    <input v-on:input="input">
+    <input v-on:input="inputPrice">
     <div class="payment">
-      <label>カレーライス</label>
+      <label>{{ item1.name }}</label>
+      <label>{{ item1.price }} yen</label>
+      <button v-on:click="buy(item1.name)">BUY</button>
+    </div>
+    <div class="payment">
+      <label>{{ itemName2 }}</label>
       <label>400円</label>
+      <button v-on:click="buy(itemName2)">BUY</button>
     </div>
   </div>
 </template>
@@ -30,5 +59,6 @@ label {
   width: 400px;
   color: black;
   background-color: aliceblue;
+  margin-bottom: 50px;
 }
 </style>
